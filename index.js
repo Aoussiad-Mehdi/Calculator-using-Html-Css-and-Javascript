4125975e71419b693efd233440a4f08a72147d2a
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-07-09 20:53:21 +0100

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+// The calculator Functionality .
+var input = document.querySelector('input');
+var int = document.querySelector('#int');
+function changeBackground(){
+    int.style.background='green';
+    int.style.transition='.4s ease'
+}
+function whiteBackground(){
+    int.style.background='white';
+    int.style.transition='.4s ease'
+}
+// More feature coming soon .
